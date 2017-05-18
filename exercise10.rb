8ace33246a83e64a9f0ae4cf3622c9da54d22648
Emily Boucher <emi.boucher@gmail.com>
2017-05-18 01:18:13 -0400

    done to ex 10 no6, bonus material and stretch exercises to be completed later

--- exercise10.rb
+++ exercise10.rb
@@ -28,8 +28,31 @@ puts "------------------------"
 #no4
 def cohort_list(students)
   students.each do |key, item|
-    print "#{key} " 
+    print "#{key} "
   end
 end
 
 cohort_list(students)
+
+puts "------------------------"
+puts "------------------------#5"
+#no5
+
+def cohort_expand(students)
+  students.each do |key, item|
+    puts "#{item * 1.05}"
+  end
+end
+
+cohort_expand(students)
+puts "------------------------#6"
+
+#no6
+def budget_cuts(students)
+  students.delete(:cohort2)
+  puts display_cohorts(students)
+end
+
+budget_cuts(students)
+
+puts "------------------------#7"
